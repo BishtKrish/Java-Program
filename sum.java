e8fdb105c5f6150c54ee62cac1a7e9dda4a3be3b
Krishna Singh Bisht <141957819+BishtKrish@users.noreply.github.com>
2023-09-14 14:46:49 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,17 @@
+class Result{
+        int sum(int a, int b)
+        {
+                int output=a+b;
+                return output;
+          
+        }
+        public static void main(String []args)
+        {
+               Result obj= new Result();
+               System.out.println(obj.sum(3,4));
+
+        }
+
+}
+
+
