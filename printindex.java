d172dac15ef7c3c0179b7939c8fce99c22b9f95a
Krishna Singh Bisht <141957819+BishtKrish@users.noreply.github.com>
2023-11-03 09:17:02 -0700

    Add files via upload

---
+++ printindex.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+ public class printindex
+{
+    public static void main(String[] args)
+    throws IOException
+    {
+        
+        String str;
+        InputStreamReader in = new InputStreamReader(System.in);
+       BufferedReader scr=new BufferedReader(in);
+        str=scr.readLine();
+        System.out.print("enter the string");
+        
+
+
+    }
+}
